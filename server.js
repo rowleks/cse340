682f19f77bfccb19dc6f2da794cb87a68d4652c5
Rowland Momoh <momohrowland@gmail.com>
2025-10-28 16:12:51 +0100

    Setup server

--- server.js
+++ server.js
@@ -1,39 +1,25 @@
-/* ******************************************
- * This server.js file is the primary file of the
- * application. It is used to control the project.
- *******************************************/
-/* ***********************
- * Require Statements
- *************************/
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
 require("dotenv").config();
 const app = express();
 const static = require("./routes/static");
-app.set("layout", "./layouts/layout");
 
-/* ***********************
- * View Engine and Templates
- *************************/
+// View Engine and Templates (Middlewares)
 app.set("view engine", "ejs");
+app.set("layout", "./layouts/layout");
 app.use(expressLayouts);
-app.set("layout", "./layouts/layout")
-
-/* ***********************
- * Routes
- *************************/
 app.use(static);
 
-/* ***********************
- * Local Server Information
- * Values from .env (environment) file
- *************************/
+// Routes
+app.get("/", (_, res) => {
+  res.render("index", { title: "Home" });
+});
+
+// env variales
 const port = process.env.PORT;
 const host = process.env.HOST;
 
-/* ***********************
- * Log statement to confirm server operation
- *************************/
+// Connection onfirmation
 app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`);
 });
