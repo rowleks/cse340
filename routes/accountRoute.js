780c2d8f4b19bba6f79427de7d1c68bf001c13c3
Rowland Momoh <momohrowland@gmail.com>
2025-11-17 23:50:47 +0100

    Add server-side validation

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -5,11 +5,17 @@ const {
   renderSignUp,
   registerAccount,
 } = require("../controllers/accountController");
+const regValidate = require("../utilities/account-validation");
 const router = express.Router();
 
 router.get("/login", utils.handleErrors(renderLogin));
 
 router.get("/register", utils.handleErrors(renderSignUp));
 
-router.post("/register", utils.handleErrors(registerAccount))
+router.post(
+  "/register",
+  regValidate.registrationRules(),
+  regValidate.checkRegData,
+  utils.handleErrors(registerAccount)
+);
 module.exports = router;
---
+++ utilities/account-validation.js
@@ -0,0 +1,58 @@
+const utils = require("./index");
+const { body, validationResult } = require("express-validator");
+
+function registrationRules() {
+  return [
+    body("account_firstname")
+      .trim()
+      .escape()
+      .isLength({ min: 3 })
+      .withMessage("Please provide a first name."),
+
+    body("account_lastname")
+      .trim()
+      .escape()
+      .isLength({ min: 3 })
+      .withMessage("Please provide a last name."),
+
+    body("account_email")
+      .trim()
+      .escape()
+      .isEmail()
+      .normalizeEmail()
+      .withMessage("A valid email is required."),
+
+    body("account_password")
+      .trim()
+      .isStrongPassword({
+        minLength: 12,
+        minLowercase: 1,
+        minUppercase: 1,
+        minNumbers: 1,
+        minSymbols: 1,
+      })
+      .withMessage("Password does not meet requirements."),
+  ];
+}
+
+async function checkRegData(req, res, next) {
+  const { account_firstname, account_lastname, account_email } = req.body;
+  let errors = [];
+  errors = validationResult(req);
+  console.log(errors);
+  if (!errors.isEmpty()) {
+    const nav = await utils.buildNav();
+    res.render("account/register", {
+      errors,
+      title: "Registration",
+      nav,
+      account_firstname,
+      account_lastname,
+      account_email,
+    });
+    return;
+  }
+  next();
+}
+
+module.exports = { registrationRules, checkRegData };
